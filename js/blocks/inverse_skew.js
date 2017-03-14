9f29c365a734a7f64d8e93db2b05db7d83893f74
lceames <lceames@gmail.com>
2017-03-14 00:36:07 -0400

    fix mismatched piece colors

--- js/blocks/inverse_skew.js
+++ js/blocks/inverse_skew.js
@@ -1,17 +1,18 @@
 import block from './block';
 
-const inverseSkew = (ctx, x, y, size) => {
+const outverseSkew = (ctx, x, y, size) => {
+  y += 1;
   for (let i = 0; i < 2; i++) {
     block(ctx, x, y, 'rgb(20, 244, 4)', size);
     x += 1;
   }
 
   x -= 1;
-  y += 1;
+  y -= 1;
   for (let i = 0; i < 2; i++) {
     block(ctx, x, y, 'rgb(20, 244, 4)', size);
     x += 1;
   }
 };
 
-export default inverseSkew;
+export default outverseSkew;
--- js/blocks/outverse_skew.js
+++ js/blocks/outverse_skew.js
@@ -1,18 +1,17 @@
 import block from './block';
 
-const outverseSkew = (ctx, x, y, size) => {
-  y += 1;
+const inverseSkew = (ctx, x, y, size) => {
   for (let i = 0; i < 2; i++) {
     block(ctx, x, y, 'rgb(244, 4, 20)', size);
     x += 1;
   }
 
   x -= 1;
-  y -= 1;
+  y += 1;
   for (let i = 0; i < 2; i++) {
     block(ctx, x, y, 'rgb(244, 4, 20)', size);
     x += 1;
   }
 };
 
-export default outverseSkew;
+export default inverseSkew;
